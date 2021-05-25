b0b776de83ceec0a97b3e66bc9328e75c13b9e8a
Marcos A. Guerine <marcosgribeiro@gmail.com>
2021-05-25 12:48:41 -0300

    Dados banco atualizados para freesqldatabase

--- conexao.php
+++ conexao.php
@@ -1,8 +1,11 @@
 <?php
-
-Define('HOST', '127.0.0.1:3307');
-Define('USUARIO', 'root');
-Define('SENHA', '142536Sj');
-Define('DB', 'vitamind');
+Define('HOST', 'sql10.freesqldatabase.com:3306');
+Define('USUARIO', 'sql10414837');
+Define('SENHA', 'ra2AZ1xXbe');
+Define('DB', 'sql10414837');
+//Define('HOST', '127.0.0.1:3306');
+//Define('USUARIO', 'root');
+//Define('SENHA', '');
+//Define('DB', 'vitamind');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar'); 
